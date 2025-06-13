134b74fe596a5fc4addc643479d51a8375cd0e54
ank1tm <mishraankit9630@gmail.com>
2025-06-13 11:20:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello, Ankit! This is your first Flask app."
 
+@app.route('/about')
+def about():
+    return "This is the about page. You're learning Flask!"
+
 if __name__ == '__main__':
     app.run(debug=True)
+
